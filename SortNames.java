dee1a59a64d4a5ca8519d50290e63de524d20e5f
BENDI MANIKANTA <84337125+MANIKANTA2662@users.noreply.github.com>
2024-05-27 13:00:01 +0400

    Update SortNames.java

--- SortNames.java
+++ SortNames.java
@@ -1,21 +1,11 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.util.*;
 public class SortNames {
     public static void main(String[] args) {
-       
         Scanner scanner = new Scanner(System.in);
-        
-        
         System.out.print("Enter the number of names: ");
         int numNames = scanner.nextInt();
-        
-        
         String[] names = new String[numNames];
-        
-        
         for (int i = 0; i < numNames; i++) {
-            System.out.print("Enter name #" + (i + 1) + ": ");
             names[i] = scanner.next();
         }
         Arrays.sort(names);
