68505248162ffbd8e65970bcdc36c585c3bf70a8
BENDI MANIKANTA <84337125+MANIKANTA2662@users.noreply.github.com>
2024-05-27 12:51:35 +0400

    Update MethodOverloadingExample.java

--- MethodOverloadingExample.java
+++ MethodOverloadingExample.java
@@ -1,9 +1,18 @@
     class Adder{  
-    static int add(int a,int b){return a+b;}  
-    static int add(int a,int b,int c){return a+b+c;}  
+    static int add(int a,int b)
+        {
+            return a+b;
+        }  
+    static int add(int a,int b,int c)
+        {
+            return a+b+c;
+        }  
     }  
-    class MethodOverloadingExample{  
-    public static void main(String[] args){  
+    class MethodOverloadingExample
+    {
+    public static void main(String[] args)
+    {  
     System.out.println(Adder.add(11,11));  
     System.out.println(Adder.add(11,11,11));  
-    }}  
+    }
+    }  
